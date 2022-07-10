03f26d9d7b94c17ec08ab019739855acac0e86c0
Roy Chan <royleochan@gmail.com>
2022-07-11 00:02:03 +0800

    feat: setup postgres db connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,19 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to champions league."}
