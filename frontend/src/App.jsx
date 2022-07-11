6644bc1e5d1a3d2c834885dea340fa3f0194f753
Roy Chan <royleochan@gmail.com>
2022-07-11 18:08:05 +0800

    refactor: migrate to cra

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "theme/theme";
+import Home from "pages/Home";
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Home />
+    </ChakraProvider>
+  );
+}
+
+export default App;
