9331ca9b5025454c12299ed133ecb3b4f34507fb
Roy Chan <royleochan@gmail.com>
2022-07-11 15:43:07 +0800

    feat: setup base FE project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: {
+      theme: path.resolve("src/theme"),
+      pages: path.resolve("src/pages"),
+      assets: path.resolve("src/assets"),
+      components: path.resolve("src/components"),
+    },
+  },
+  plugins: [react()],
+});
