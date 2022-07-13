aa2d0e41f7ed57a16f364df8910cb4cde7814265
Roy Chan <royleochan@gmail.com>
2022-07-14 01:21:04 +0800

    feat: implement result calculation logic

--- backend/app/routers/operation_routes.py
+++ backend/app/routers/operation_routes.py
@@ -1,5 +1,4 @@
 
-from typing import List
 from fastapi import Depends, APIRouter
 from sqlalchemy.orm import Session
 
@@ -16,8 +15,45 @@ router = APIRouter(
 
 
 @router.delete("/reset")
-async def compute_results(db: Session = Depends(get_db)):
+async def reset(db: Session = Depends(get_db)):
     match_repository.delete_all(db)
     team_repository.delete_all(db)
 
     return {"message": "Successfully reset data."}
+
+
+@router.get("/results")
+async def compute_results(db: Session = Depends(get_db)):
+    teams = team_repository.get_all(db)
+    matches = match_repository.get_all(db)
+    group_one = {team.name: TeamResult(
+        team.name, team.registration_date, team.group) for team in teams if team.group == 1}
+    group_two = {team.name: TeamResult(
+        team.name, team.registration_date, team.group) for team in teams if team.group == 2}
+
+    for match in matches:
+        group = group_one if match.team_one in group_one and match.team_two in group_one else group_two
+        team_one = group[match.team_one]
+        team_two = group[match.team_two]
+
+        team_one.add_goals(match.team_one_goals)
+        team_two.add_goals(match.team_two_goals)
+        if match.team_one_goals > match.team_two_goals:
+            team_one.add_win()
+            team_two.add_loss()
+        elif match.team_one_goals < match.team_two_goals:
+            team_one.add_loss()
+            team_two.add_win()
+        else:
+            team_one.add_draw()
+            team_two.add_draw()
+
+    group_one_lst = [team for team in group_one.values()]
+    group_two_lst = [team for team in group_one.values()]
+    group_one_lst.sort(reverse=True)
+    group_two_lst.sort(reverse=True)
+
+    return {
+        "one": [vars(team) for team in group_one_lst],
+        "two": [vars(team) for team in group_two_lst]
+    }
--- backend/app/schemas/operation_schema.py
+++ backend/app/schemas/operation_schema.py
@@ -1,9 +1,54 @@
-from .common import ApiBase
+from datetime import datetime
+
+WIN_POINTS = 3
+DRAW_POINTS = 1
+LOSS_POINTS = 0
+ALT_WIN_POINTS = 5
+ALT_DRAW_POINTS = 3
+ALT_LOSS_POINTS = 1
 
 
-class TeamResult(ApiBase):
-    team: str
-    wins: int
-    losses: int
-    draws: int
-    points: int
+class TeamResult():
+    def __init__(self, team: str, date: str, group: int):
+        self.team = team
+        self.date = date
+        self.group = group
+        self.wins = 0
+        self.losses = 0
+        self.draws = 0
+        self.goals = 0
+        self.points = 0
+        self.alt_points = 0
+
+    def _parse_date(self, date):
+        return datetime.strptime(date, "%d/%m")
+
+    def add_goals(self, goals):
+        self.goals += goals
+
+    def add_win(self):
+        self.wins += 1
+        self.points += WIN_POINTS
+        self.alt_points += ALT_WIN_POINTS
+
+    def add_draw(self):
+        self.draws += 1
+        self.points += DRAW_POINTS
+        self.alt_points += ALT_DRAW_POINTS
+
+    def add_loss(self):
+        self.losses += 1
+        self.points += LOSS_POINTS
+        self.alt_points += ALT_LOSS_POINTS
+
+    def __gt__(self, other):
+        if self.points == other.points:
+            if self.goals == other.goals:
+                if self.alt_points == other.alt_points:
+                    return self._parse_date(self.date) < self._parse_date(other.date)
+                else:
+                    return self.alt_points > other.alt_points
+            else:
+                return self.goals > other.goals
+        else:
+            return self.points > other.points
